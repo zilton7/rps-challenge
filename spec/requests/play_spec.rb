6822973b671986b38869ef8638282692077260f1
Zil Norvilis <zilasino27@gmail.com>
2022-05-23 10:06:37 +0300

    Add tests

--- spec/requests/play_spec.rb
+++ spec/requests/play_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Play", type: :request do
   describe "GET /play/choose" do
-     it "starts the game" do
-        get '/play/choose/'
-        expect(response).to have_http_status(:success)
-        expect(response.body).to match("Choose Your Throw")
-     end
+    it "starts the game" do
+      get '/play/choose/'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to match("Choose Your Throw")
+    end
 
     it "determines winner" do
       get '/play/choose/?choice=Rock'
